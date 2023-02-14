da30ad1740a5c42ceeef273d3ce26ba226b2a519
Guilherme Pinheiro <guiapinheiro29@gmail.com>
2023-02-14 13:16:12 -0300

    some changes and the start

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 import Component from '../Content';
 
+import img from '../../images/bg-main-desktop.png';
+
 import './styles.css';
 
 export default function Main() {
   return (
     <>
       <main>
+        <img src={img} alt="main-img-desktop" />
         <Component />
       </main>
     </>
